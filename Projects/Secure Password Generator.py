c568c4413e6782689ac02b81652a03091a2e21d0
Hungry15230 <117875463+Hungry715230@users.noreply.github.com>
2023-01-20 14:31:56 -0500

    Update Secure Password Generator.py

--- Projects/Secure Password Generator.py
+++ Projects/Secure Password Generator.py
@@ -1,9 +1,9 @@
 import random
 import string
-action = input("Do you want a secure password? (y / n)")
-if action.lower() == "y" or "yes":
+action=input("Do you want a secure password? (y / n)")
+if action.lower()=="y"or action.lower()=="yes":
     print("Okay\n")
-    randomString = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation ) for n in range(20)])
+    randomString=''.join([random.choice(string.ascii_letters+string.digits+string.punctuation)for n in range(20)])
     print(randomString)
 else:
     print("\nHere is your unsecure password:\n\nAB123456789")
