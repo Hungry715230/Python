b8b730bac3f2b6e1e42d8f161f2e68366f441595
Hungry15230 <117875463+Hungry715230@users.noreply.github.com>
2023-01-23 14:13:52 -0500

    Create To-Do List.py

---
+++ Projects/To-Do List.py
@@ -0,0 +1,30 @@
+import string
+
+ToDoList = []
+
+print("This is your personal To-Do list.\nYour actions are to add, remove, sort, view list and mark done.\nType 'help' for help\n")
+while True:
+    action = input("What action would you like to take?")
+    if action.lower() == "help":
+        print("To add type 'add'")
+        print("To remove type 'remove'")
+        print("To mark done type 'done'")
+        print("To sort list type 'sort'")
+        print("To view list type 'view' or type 'list'\n")
+    if action.lower() == "add":
+        add = input("\nWhat would you like to add?")
+        ToDoList.append(add)
+        print(f"{add} was added to your list.\n")
+    if action.lower() == "list" or action.lower() == "view":
+        for element in ToDoList:
+            print(f"{element}")
+        print(" ")
+    if action.lower() == "sort":
+        sort(ToDoList)
+        print("\nYour list was sorted.\n")
+    if action.lower() == "remove":
+        removing = input("\nWhat would you like to remove?")
+        removing = removing.lower()
+        if removing in ToDoList:
+            ToDoList.remove(removing)
+            print(f"{removing} was removed.\n")
